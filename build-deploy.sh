1c1e9bbff59787761900ac4ac00913dc7a4f24eb
Gianguido Sora <g.sora4@gmail.com>
2018-02-23 00:03:17 +0100

    small script to clean build and deploy via novacom

---
+++ build-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# build and deploy moboot via novacom
+make clean
+make -j$(nproc)
+echo "Press enter when ready to deploy..."
+read enter
+sleep 3
+novacom boot mem:// < build-pre3/lk.uImage
