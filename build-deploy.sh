ebefe6ec617b8861112c7a2855bbe9f35f211f7e
Gianguido Sora` <g.sora4@gmail.com>
2018-02-24 13:35:37 +0100

    exit if make errors out

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # build and deploy moboot via novacom
-make clean
-make -j$(nproc)
-echo "Press enter when ready to deploy..."
-read enter
-sleep 3
-novacom boot mem:// < build-pre3/lk.uImage
+make clean; make -j$(nproc) && echo "Press enter when ready to deploy..." && read enter && sleep 3 && novacom boot mem:// < build-pre3/lk.uImage
